733bfaaec66e3fd7c16913afe857c4ddac311656
mdg1317 <matthew.dennis137@gmail.com>
2020-03-23 21:00:01 -0700

    Added tests to the rest of the methods (checkpoint 3)

--- app/src/test/java/up/edu/dimcalc/TwoPointsTest.java
+++ app/src/test/java/up/edu/dimcalc/TwoPointsTest.java
@@ -14,7 +14,7 @@ public class TwoPointsTest {
         TwoPoints testPoints = new TwoPoints();
         Point p1 = testPoints.getPoint(0);
         Point p2 = testPoints.getPoint(1);
-        assertEquals(10, p1.x);
+        assertEquals(0, p1.x);
         assertEquals(0, p1.y);
         assertEquals(0, p2.x);
         assertEquals(0, p2.y);
@@ -38,21 +38,66 @@ public class TwoPointsTest {
 
     @Test
     public void randomValue() {
+        TwoPoints testPoints = new TwoPoints();
+        testPoints.randomValue(0);
+        testPoints.randomValue(1);
+        Point p1 = testPoints.getPoint(0);
+        Point p2 = testPoints.getPoint(1);
+        assertTrue(0 <= p1.x);
+        assertTrue(0 <= p1.y);
+        assertTrue(0 <= p2.x);
+        assertTrue(0 <= p2.y);
     }
 
     @Test
     public void setOrigin() {
+        TwoPoints testPoints = new TwoPoints();
+        testPoints.setOrigin(0);
+        testPoints.setOrigin(1);
+        Point p1 = testPoints.getPoint(0);
+        Point p2 = testPoints.getPoint(1);
+        assertEquals(0, p1.x);
+        assertEquals(0, p1.y);
+        assertEquals(0, p2.x);
+        assertEquals(0, p2.y);
     }
 
     @Test
     public void copy() {
+        TwoPoints testPoints = new TwoPoints();
+        testPoints.setPoint(0, 10, -3);
+        Point p1 = testPoints.getPoint(0);
+        testPoints.copy(0, 1);
+        Point p2 = testPoints.getPoint(1);
+        assertEquals(10, p2.x);
+        assertEquals(-3, p2.y);
     }
 
     @Test
     public void distance() {
+        TwoPoints testPoints = new TwoPoints();
+        testPoints.randomValue(0);
+        testPoints.randomValue(1);
+        Point p1 = testPoints.getPoint(0);
+        Point p2 = testPoints.getPoint(1);
+        double distance = testPoints.distance();
+        int xDiff = p2.x - p1.x;
+        int yDiff = p2.y - p1.y;
+        double distanceEx = Math.sqrt((xDiff*xDiff) + (yDiff*yDiff));
+        assertEquals(distanceEx, distance, 0.1);
     }
 
     @Test
     public void slope() {
+        TwoPoints testPoints = new TwoPoints();
+        testPoints.randomValue(0);
+        testPoints.randomValue(1);
+        Point p1 = testPoints.getPoint(0);
+        Point p2 = testPoints.getPoint(1);
+        double slope = testPoints.slope();
+        int xDiff = p2.x - p1.x;
+        int yDiff = p2.y - p1.y;
+        double slopeEx = yDiff / xDiff;
+        assertEquals(slopeEx, slope, 0.1);
     }
 }
